9c82d02bafe0e14b46591e611ffcda4ddf25dbab
Wordbe <seonghojin3@gmail.com>
2021-03-18 22:20:55 +0900

    fix: kubernetes typo fixed

--- src/main/java/co/wordbe/thejavaspringboot/post/PostController.java
+++ src/main/java/co/wordbe/thejavaspringboot/post/PostController.java
@@ -3,12 +3,6 @@ package co.wordbe.thejavaspringboot.post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.availability.ApplicationAvailability;
-import org.springframework.boot.availability.AvailabilityChangeEvent;
-import org.springframework.boot.availability.LivenessState;
-import org.springframework.boot.availability.ReadinessState;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.util.unit.DataSize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,31 +30,5 @@ public class PostController {
         System.out.println(exampleData);
         System.out.println(exampleData.toMegabytes());
         return "hello";
-    }
-
-
-    @Autowired
-    ApplicationEventPublisher eventPublisher;
-
-    @GetMapping("/turnoff")
-    public String turnoff() {
-        AvailabilityChangeEvent.publish(eventPublisher, this, LivenessState.BROKEN);
-        return "Broken";
-    }
-
-    @GetMapping("/block")
-    public String block() {
-        AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.REFUSING_TRAFFIC);
-        return "Blocked requests";
-    }
-
-    @Async
-    @EventListener
-    public void onStateChanged(AvailabilityChangeEvent<ReadinessState> readiness) throws InterruptedException {
-        System.out.println("State is changed to " + readiness.getState());
-        if (readiness.getState() == ReadinessState.REFUSING_TRAFFIC) {
-            Thread.sleep(5000L);
-            AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.ACCEPTING_TRAFFIC);
-        }
     }
 }
--- src/main/java/co/wordbe/thejavaspringboot/service/ServiceController.java
+++ src/main/java/co/wordbe/thejavaspringboot/service/ServiceController.java
@@ -2,6 +2,12 @@ package co.wordbe.thejavaspringboot.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +25,30 @@ public class ServiceController {
         return "Appliation is now " + availability.getLivenessState()
                 + " " + availability.getReadinessState()
                 + " " + localHostService.getLocalHostInfo();
+    }
+
+    @Autowired
+    ApplicationEventPublisher eventPublisher;
+
+    @GetMapping("/turnoff")
+    public String turnoff() {
+        AvailabilityChangeEvent.publish(eventPublisher, this, LivenessState.BROKEN);
+        return "Broken";
+    }
+
+    @GetMapping("/block")
+    public String block() {
+        AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.REFUSING_TRAFFIC);
+        return "Blocked requests";
+    }
+
+    @Async
+    @EventListener
+    public void onStateChanged(AvailabilityChangeEvent<ReadinessState> readiness) throws InterruptedException {
+        System.out.println("State is changed to " + readiness.getState());
+        if (readiness.getState() == ReadinessState.REFUSING_TRAFFIC) {
+            Thread.sleep(5000L);
+            AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.ACCEPTING_TRAFFIC);
+        }
     }
 }
